77307ad70bd7d22138ee2b2c83acfea6550b133d
FifiLen <filippraca02@gmail.com>
2024-04-25 14:43:59 +0200

    hide

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <main>
       
       
-      <Hero />
-      <ExpandedSection />
+      {/* <Hero /> */}
+      {/* <ExpandedSection /> */}
       <Courses />
       {/* <About /> */}
 
